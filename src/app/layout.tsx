3fda45e4efd04dce8453b7920fd3dcf2c9c575b0
Eso70 <bahrambedl1994@gmail.com>
2026-03-08 00:19:53 +0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { getAppUrl } from "@/lib/app-url";
-import { TikTokPixel } from "@/components/analytics/TikTokPixel";
 
 const primaryFont = localFont({
   src: "../../public/fonts/kurdishFont.woff",
@@ -65,7 +64,21 @@ export default function RootLayout({
       <head>
         <link rel="dns-prefetch" href="https://analytics.tiktok.com" />
         <link rel="preconnect" href="https://analytics.tiktok.com" crossOrigin="anonymous" />
-        <TikTokPixel />
+        <script
+          id="tiktok-pixel"
+          dangerouslySetInnerHTML={{
+            __html: `!function (w, d, t) {w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;n.onerror=function(){};e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};ttq.load('D6M52Q3C77U160FIC8M0');ttq.page();}(window, document, 'ttq');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://analytics.tiktok.com/i18n/pixel/events.js?sdkid=D6M52Q3C77U160FIC8M0&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
       <body
         suppressHydrationWarning
